bf8291b2e48937c5ff99ddbdeb737d23de89672c
J <j@war.io>
2022-04-03 14:51:43 -0400

    Testing more color themes..

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -228,6 +228,16 @@ values."
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(spacemacs-dark
+                         alect-black
+                         ample-zen
+                         alect-black
+                         ir-black
+                         junio
+                         lush
+                         manoj-dark
+                         reverse
+                         soothe
+                         wheatgrass
                          organic-green
                          solarized-dark
                          tango-dark
@@ -236,6 +246,7 @@ values."
                          solarized-light
                          leuven
                          zenburn)
+
    ;; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
 
